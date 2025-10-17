23e4d10289e3fbfe69cbe89420d09fce4d67b27b
siyuxie2025 <12345678siyuxie2025@users.noreply.github.com>
2025-10-17 14:01:14 -0400

    Added methods.py to include a ForcedSamplingBandit abstract class and two realizations

--- src/methods.py
+++ src/methods.py
@@ -1,14 +1,74 @@
-# class definition
 import numpy as np
-import pandas as pd
 from quantes.linear import low_dim
-from scipy.stats import norm
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-from tqdm import tqdm
-import os
 
-class RiskAwareBandit:
+from abc import ABC, abstractmethod
+
+class ForcedSamplingBandit(ABC):
+    '''
+    Abstract base class for Bandit algorithms with forced sampling and all-sample estimators.
+    '''
+    @abstractmethod
+    def __init__(self, q, h, d, K, **kwargs):
+        '''
+        Initialize the Bandit algorithm.
+
+        Parameters
+        ----------
+        q : int
+            Number of forced samples per arm in each round
+        h : float
+            Difference between optimal and suboptimal arms
+        d : int
+            Dimension of the context vectors
+        K : int
+            Number of arms
+        '''
+        pass
+    
+    @property
+    @abstractmethod
+    def choose_a(self, t, x):
+        '''
+        Choose an action based on the current time step and context vector.
+
+        Parameters
+        ----------
+        t : int
+            The current time step.
+        x : np.ndarray
+            The context vector of dimension d.
+
+        Returns
+        -------
+        int
+            The index of the chosen action.
+        '''
+        pass
+
+    @property
+    @abstractmethod
+    def update_beta(self, rwd, t):
+        '''
+        Update the estimators based on the received reward.
+
+        Parameters
+        ----------
+        rwd : float
+            The reward received after taking the action.
+        t : int
+            The current time step.
+
+        Returns
+        -------
+        None
+        '''
+        pass
+
+
+
+
+class RiskAwareBandit(ForcedSamplingBandit):
     '''
     A class for the Risk-Aware Bandit algorithm with forced sampling and all-sample estimators.
     '''
@@ -17,6 +77,7 @@ class RiskAwareBandit:
         Initialize the Risk-Aware Bandit algorithm.
 
         Parameters:
+        ----------
         q (int) : Number of forced samples per arm in each round
         h (float) : Difference between optimal and suboptimal arms
         tau (float) : Quantile level for quantile regression
@@ -49,15 +110,32 @@ class RiskAwareBandit:
         self.beta_error_t = np.zeros(K)
 
     
-    def choose_a(self,t,x): #x is d-dim vector
-        # if t is the first time of the new round
-        '''
+    def choose_a(self,t,x): 
+        """ 
+        Choose an action based on the current time step and context vector.
         
-        '''
+        If the current time step is part of the forced sampling set,
+        select the corresponding action. Otherwise, use the estimators
+        to choose the action with the highest estimated reward within
+        the acceptable range.
+        Otherwise, select the action with the highest estimated reward.
+
+        Parameters
+        ----------
+        t : int
+            The current time step.
+        x : np.ndarray
+            The context vector of dimension d.
+        Returns
+        -------
+        int
+            The index of the chosen action.
+        """
+        # if t is the first time of the new round
         if t == ((2**self.n - 1)*self.K*self.q + 1):
             self.set = np.arange(t, t+self.q*self.K)
             self.n += 1
-        if t in self.set: # forced sampling
+        if t in self.set: 
             ind = list(self.set).index(t)
             self.action = ind // self.q
             self.Tx[self.action].append(x)
@@ -66,15 +144,28 @@ class RiskAwareBandit:
             max_forced_est = np.amax(forced_est)
             K_hat = np.where(forced_est > max_forced_est - self.h/2.)[0]
             all_est = [np.dot(self.beta_a[k_hat], x) + self.alpha_a[k_hat] for k_hat in K_hat]
-            print(f"K_hat: {K_hat}, all_est: {all_est}")  ## print the result
+            # print(f"K_hat: {K_hat}, all_est: {all_est}")  ## print the result
             self.action = K_hat[np.argmax(all_est)]
 
         self.Sx[self.action].append(x)
 
         return self.action
     
-    # update beta
     def update_beta(self, rwd, t):
+        """ Update the estimators based on the received reward.
+
+        For the first d samples, random initialization is used.
+
+        Parameters
+        ----------
+        rwd : float
+            The reward received after taking the action.
+        t : int
+            The current time step.
+        Returns
+        -------
+        None
+        """
         if np.array(self.Tx[self.action]).shape[0] > self.d:
             if t in self.set:
                 self.Tr[self.action].append(rwd)
@@ -90,7 +181,6 @@ class RiskAwareBandit:
                             intercept=True).fit(tau=self.tau)
             self.beta_a[self.action] = all_qr['beta'][1:]
             self.alpha_a[self.action] = all_qr['beta'][0]
-            # print(f"Shapes - Tx: {np.array(self.Tx[self.action]).shape}, Tr: {np.array(self.Tr[self.action]).shape}")
             self.beta_error_a[self.action] = np.linalg.norm(self.beta_a[self.action] - self.beta_real_value[self.action])
             self.beta_error_t[self.action] = np.linalg.norm(self.beta_t[self.action] - self.beta_real_value[self.action])
             return
@@ -107,3 +197,77 @@ class RiskAwareBandit:
             return 
         
 
+class OLSBandit(ForcedSamplingBandit):
+    def __init__(self, q, h, d, K, beta_real_value):
+        """Initialize the OLSBandit with given parameters.
+        """
+        self.Sx = np.empty((K, 0)).tolist()
+        self.Sr = np.empty((K, 0)).tolist()
+        self.Tx = np.empty((K, 0)).tolist()
+        self.Tr = np.empty((K, 0)).tolist()
+
+        self.beta_a = np.random.uniform(0., 2., (K, d))
+        self.beta_t = np.random.uniform(0., 2., (K, d))
+
+        self.beta_real_value = beta_real_value
+
+        self.beta_error_a = np.zeros(K)
+        self.beta_error_t = np.zeros(K)
+
+        self.q = q
+        self.h = h
+        self.d = d
+        self.K = K
+
+        self.n = 0
+
+    def choose_a(self, t, x):
+        """Choose an action based on the current time step and context vector.
+        """
+        if t == ((2**self.n - 1)*self.K*self.q + 1):
+            self.set = np.arange(t, t+self.q*self.K)
+            self.n += 1
+        if t in self.set:
+            ind = list(self.set).index(t)
+            self.action = ind // self.q
+            self.Tx[self.action].append(x)
+        else:
+            # no intercept
+            forced_est = np.dot(self.beta_t, x)
+            max_forced_est = np.amax(forced_est)
+            K_hat = np.where(forced_est > max_forced_est - self.h/2.)[0]
+            all_est = [np.dot(self.beta_a[k_hat], x) for k_hat in K_hat]
+            self.action = K_hat[np.argmax(all_est)]
+            
+        self.Sx[self.action].append(x)
+
+        return self.action
+
+
+    def update_beta(self, rwd, t):
+        """Update the estimators based on the received reward.
+        """
+        if np.array(self.Tx[self.action]).shape[0] > self.d:
+            if t in self.set:
+                self.Tr[self.action].append(rwd)
+                forced_ols = LinearRegression(fit_intercept=False)
+                forced_ols.fit(np.array(self.Tx[self.action]), np.array(self.Tr[self.action]))
+                self.beta_t[self.action] = forced_ols.coef_
+
+            self.Sr[self.action].append(rwd)
+            all_ols = LinearRegression(fit_intercept=False)
+            all_ols.fit(np.array(self.Sx[self.action]), np.array(self.Sr[self.action]))
+            self.beta_a[self.action] = all_ols.coef_
+
+            self.beta_error_a[self.action] = np.linalg.norm(self.beta_a[self.action] - self.beta_real_value[self.action])
+            self.beta_error_t[self.action] = np.linalg.norm(self.beta_t[self.action] - self.beta_real_value[self.action])
+            return
+        else:
+            self.Tr[self.action].append(rwd)
+            self.Sr[self.action].append(rwd)
+            self.beta_t[self.action] = np.random.uniform(0., 2., self.d)
+            self.beta_a[self.action] = np.random.uniform(0., 2., self.d)
+
+            self.beta_error_a[self.action] = np.linalg.norm(self.beta_a[self.action] - self.beta_real_value[self.action])
+            self.beta_error_t[self.action] = np.linalg.norm(self.beta_t[self.action] - self.beta_real_value[self.action])
+            return
